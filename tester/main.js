da4f743691b8482432628a1b57e26b18f3aab8c9
Pinnapong Silpsakulsuk <pinnapong@acommerce.asia>
2017-01-12 17:35:14 +0700

    Add tester module

---
+++ tester/main.js
@@ -0,0 +1,11 @@
+const request = require('request')
+const n = process.argv[2] || 1
+const start = (new Date).getTime()
+
+for(let i=0; i < n; i++) {
+  request('http://localhost:8000', (error, response, body) => {
+    const elapsed = (new Date).getTime() - start
+    console.log(`Received response ${i+1} at ${elapsed}ms`)
+  })
+}
+
